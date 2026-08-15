398331290793a399c664c599cd8d5fe192ab1884
TahsinOhi <tahsinohi23@gmail.com>
2026-08-15 11:34:16 +0600

    Update Register, FindRoot, style4, App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import FindRoot from "./pages/FindRoot";
+import Registration from "./pages/Register";
+
+
+function App() {
+  return (
+    
+    //<FindRoot />
+        <Registration />
+          
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,35 @@
+import "../../style4.css";
+
+
+function Registration() {
+    return (
+    
+ <div className="Registrationbox">
+
+<div className="Registrationheader">
+    <h3>Registration</h3>
+</div>
+<div className="inputbox">
+   <input  className="input-field" type="text" placeholder="Name"/>
+</div>
+<div className="inputbox">
+   <input  className="input-field" type="text" placeholder="Email"/>
+</div>
+<div className="inputbox">
+   <input className="input-field" type="password" placeholder="password"/>
+</div>
+<div className="inputbox">
+   <input className="input-field" type="password" placeholder="confirm password"/>
+</div>
+ <div className="inputbox">
+    <button className="submitbtn">Register</button>
+</div>
+
+   
+     
+</div>
+    );
+}
+
+export default Registration;
+    
