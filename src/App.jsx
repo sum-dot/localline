c79d6d88d38df8c693fc3f5ed3c8f01fbb6e5d1d
Tasin <tasinchowdhury223@gmail.com>
2026-08-16 00:55:00 +0600

    Login & routeFinder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 
 import './App.css'
+//import FindRoot from "./pages/FindRoot";
+//import Registration from "./pages/Register";
+
+//import BusSearch from "./pages/BusSearch";
+//import Profile from "./pages/Profile";
 
 import Login from "./pages/Login";
-import RouteFinder from "./pages/RouteFinder";
+//import RouteFinder from "./pages/RouteFinder";
 
 function App() {
     return (
         <div >
-              <Login /> 
-          {/* <RouteFinder />*/}
+             <Login />
+            {/*
+            <FindRoot />
+            <RouteFinder />
+            */}
+            {/*<Registration />*/}
         </div>
     );
+
 }
 
 export default App;
