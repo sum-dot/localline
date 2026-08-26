8c90baa158c11f77c1da7f7e75e0e39994ccf3ab
Maliha <sfmaliha12@gmail.com>
2026-08-26 18:33:08 +0600

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css'
-//import FindRoot from "./pages/FindRoot";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FindRoot from "./pages/FindRoot";
+import RouteFinder from "./pages/RouteFinder";
+
 //import Registration from "./pages/Register";
 
-
 //import Login from "./pages/Login";
-//import RouteFinder from "./pages/RouteFinder";
 
-//import BusSearch from "./pages/BusSearch";
-//import BusSearchResult from "./pages/BusSearchResult";
-import Profile from "./pages/Profile";
+
+import BusSearch from "./pages/BusSearch";
+import BusSearchResult from "./pages/BusSearchResult";
+//import Profile from "./pages/Profile";
 
 function App() {
     return (
       <div>
         {/* <Login /> */}
-        {/* <FindRoot /> */}
-        {/* <RouteFinder /> */}
+         <FindRoot />
+         <RouteFinder /> 
+        {/* <BusSearch />   */}
+         {/* <BusSearchResult />  */}
         {/*<Registration />*/}
-        {/*<BusSearch />  */}
-        {/*<BusSearchResult /> */}
-         <Profile />
+
+        {/* <Profile />*/}
       </div>
     );
 
